e945b64dc33aea0ccbe45577a54e78d9a790f7c5
xmonarch <xmonarch64@gmail.com>
2020-09-17 08:30:13 +0200

    added lightdm configs

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-read -p "WARNING: This may overwrite your current configs. Proceed? [y/N]" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	sudo cp wallpaper/florest-stair2-mola.jpg /usr/share/pixmaps/wallpaper.jpg
-	ln -sfn $PWD/termite ~/.config/termite
-	ln -sfn $PWD/picom ~/.config/picom
-	ln -sfn $PWD/fish ~/.config/fish
-	ln -sfn $PWD/i3 ~/.config/i3
-	ln -sfn $PWD/polybar ~/.config/polybar
-	ln -sfn $PWD/dunst ~/.config/dunst
+if [ -z "$1" ]; then
+    echo "WARNING: This may overwrite your current configs. Add -f to proceed"
+    exit 1
 fi
+
+sudo cp wallpaper/florest-stair2-mola.jpg /usr/share/pixmaps/wallpaper.jpg
+sudo cp lightdm/lightdm.conf /etc/lightdm/
+sudo cp lightdm/lightdm-mini-greeter.conf /etc/lightdm/
+ln -sfn $PWD/termite ~/.config/termite
+ln -sfn $PWD/picom ~/.config/picom
+ln -sfn $PWD/fish ~/.config/fish
+ln -sfn $PWD/i3 ~/.config/i3
+ln -sfn $PWD/polybar ~/.config/polybar
+ln -sfn $PWD/dunst ~/.config/dunst
+
